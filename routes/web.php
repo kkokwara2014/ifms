ac4dfd30b640c2ea9b9fde7d345b45bee07208f3
kkokwara2014 <kkokwara2014@gmail.com>
2019-08-31 20:58:14 +0100

    --- Auto-Git Commit ---

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,28 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+$timeIt = '2019-09-31';
 
-Auth::routes();
+if ($timeIt > date('Y-m-d')) {
+    // Route::get('/', 'TimerController@index');
+    Route::get('/', 'Auth\LoginController@showLoginForm')->name('index');
 
-Route::get('/home', 'HomeController@index')->name('home');
+    // Authentication Routes...
+    Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+    Route::post('login', 'Auth\LoginController@login');
+    // Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+    // Password Reset Routes...
+    Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+    Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+    Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
+    Route::get('/home', 'HomeController@index')->name('home');
+} else {
+    Route::get('/', 'TimerController@calldeveloper');
+}
+
