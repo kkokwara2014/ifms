24e26c242e5e5eee8bc48f44d1cac58c069ca400
kkokwara2014 <kkokwara2014@gmail.com>
2019-09-04 12:25:13 +0100

    --- Auto-Git Commit ---

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -1,20 +1,17 @@
 <!DOCTYPE html>
 <html>
-
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>IFMS Dashboard</title>
+    <title>IFMS | Dashboard</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.7 -->
     <link rel="stylesheet" href="{{asset('bootstrap_assets/bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="{{asset('bootstrap_assets/bower_components/font-awesome/css/font-awesome.min.css')}}">
-    {{-- <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"> --}}
     <!-- Ionicons -->
     <link rel="stylesheet" href="{{asset('bootstrap_assets/bower_components/Ionicons/css/ionicons.min.css')}}">
-    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/4.5.6/css/ionicons.min.css"> --}}
     <!-- Theme style -->
     <link rel="stylesheet" href="{{asset('bootstrap_assets/dist/css/AdminLTE.min.css')}}">
     <!-- AdminLTE Skins. Choose a skin from the css/skins
@@ -31,9 +28,8 @@
     <link rel="stylesheet"
       href="{{asset('bootstrap_assets/bower_components/bootstrap-daterangepicker/daterangepicker.css')}}">
     <!-- bootstrap wysihtml5 - text editor -->
-    <link rel="stylesheet" href="{{asset('bootstrap_assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css')}}">
-    <!-- DataTables -->
-  <link rel="stylesheet" href="{{asset('bootstrap_assets/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css')}}">
+    <link rel="stylesheet"
+      href="{{asset('bootstrap_assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css')}}">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -56,7 +52,7 @@
           <!-- mini logo for sidebar mini 50x50 pixels -->
           <span class="logo-mini"><b>A</b>LT</span>
           <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><b>IFMS</b></span>
+          <span class="logo-lg"><b>IFMS</b> Dashboard</span>
         </a>
         <!-- Header Navbar: style can be found in header.less -->
         <nav class="navbar navbar-static-top">
@@ -71,22 +67,22 @@
               <!-- User Account: style can be found in dropdown.less -->
               <li class="dropdown user user-menu">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                  <img src="{{asset('bootstrap_assets/dist/img/user2-160x160.jpg')}}" class="user-image" alt="User Image">
-                  <span class="hidden-xs">{{ Auth::user()->name }}</span>
+                  <img src="{{asset('bootstrap_assets/dist/img/user2-160x160.jpg')}}" class="user-image"
+                    alt="User Image">
+                  <span class="hidden-xs">{{Auth::user()->name}}</span>
                 </a>
                 <ul class="dropdown-menu">
                   <!-- User image -->
                   <li class="user-header">
-                    <img src="{{asset('bootstrap_assets/dist/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image">
+                    <img src="{{asset('bootstrap_assets/dist/img/user2-160x160.jpg')}}" class="img-circle"
+                      alt="User Image">
 
                     <p>
-                      {{ Auth::user()->name }} - Admin
-                      <small>Member since {{Auth::user()->created_at->diffForHumans() }}</small>
+                      {{Auth::user()->name}} - Super Admin
+                      <small>Member since {{Auth::user()->created_at->diffForHumans()}}</small>
                     </p>
                   </li>
-                  <!-- Menu Body -->
-                  <li class="user-body">
-                  </li>
+
                   <!-- Menu Footer-->
                   <li class="user-footer">
                     <div class="pull-left">
@@ -98,25 +94,7 @@
                   </li>
                 </ul>
               </li>
-              {{-- <!-- Control Sidebar Toggle Button -->
-              <li>
-                <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
-              </li> --}}
             </ul>
           </div>
         </nav>
       </header>
-
-      <!-- Content Wrapper. Contains page content -->
-      <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-          <h1>
-            {{ $pageTitle }}
-            <small>Control panel</small>
-          </h1>
-          {{-- <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li class="active">Dashboard</li>
-          </ol> --}}
-        </section>
