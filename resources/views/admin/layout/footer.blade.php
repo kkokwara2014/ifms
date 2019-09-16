1c04f1fdf55c318e2a8ff0c7551d945d54daf48b
kkokwara2014 <kkokwara2014@gmail.com>
2019-09-16 11:03:22 +0100

    --- Auto-Git Commit ---

--- resources/views/admin/layout/footer.blade.php
+++ resources/views/admin/layout/footer.blade.php
@@ -2,7 +2,7 @@
   <div class="pull-right hidden-xs">
     <b>Version</b> 2.4.0
   </div>
-  <strong>Copyright &copy; 2018 ifms.</strong> All rights
+  <strong>Copyright &copy; 2019 ifms.</strong> All rights
   reserved.
 </footer>
 
@@ -248,6 +248,7 @@
   $(function () {
       $('#example1').DataTable({
         'sort':false,
+        'responsive':true,
       })
       $('#example2').DataTable({
         'paging'      : true,
